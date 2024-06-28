5d8499c15c310b71079c0e0effc60f315ffbfc2f
javan <javanluiz757@gmail.com>
2024-06-28 13:15:37 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <main className="container bg-black">
+        <div className="w-4 h-4 bg-red-700"></div>
+      </main>
+    </>
+  );
+}
+
+export default App;
