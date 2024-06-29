c3a4ca1ce005cfc15ad83ca0f4e0b894586c65bd
javan <javanluiz757@gmail.com>
2024-06-29 11:28:51 -0300

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
+import NavBar from "./components/nav-bar/navBar";
+
 function App() {
   return (
-    <>
-      <main className="container bg-black">
-        <div className="w-4 h-4 bg-red-700"></div>
+    <div className=" w-screen h-screen bg-slate-700">
+      <NavBar />
+      <main className="container w-full h-screen bg-blue-950 ">
+        <div className="w-full h-72 flex justify-center items-center p-4 bg-black mt-8">
+          <div className="min-w-32 h-32 bg-orange-400 rounded-full"></div>
+          <p className="text-white ml-6 text-xs">
+            <b>AúdioBook</b>
+            <br /> dolor sit amet consectetur adipisicing elit. Similique vel
+            sed facilis veritatis delectus,{" "}
+          </p>
+        </div>
       </main>
-    </>
+      <div className="curtain w-full fixed top-96 h-full bg-orange-400"></div>
+    </div>
   );
 }
 
---
+++ src/components/nav-bar/navBar.jsx
@@ -0,0 +1,9 @@
+import "./style.css";
+
+export default function NavBar() {
+  return (
+    <header>
+      <nav className=" bg-orange-400 h-20"></nav>
+    </header>
+  );
+}
